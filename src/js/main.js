de3a626efb1f08e86f19f01f2cbbb6700ad62738
nathan-www <55201769+nathan-www@users.noreply.github.com>
2019-09-12 20:18:30 +0100

    Fixing missing functions

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@ var balgbp = 0; // set defult balance to 0
 var balaio = 0;
 var nodeip;
 var aioprice = 0.80; // static price of £0.80 untill we get a exchange (as this is arround the target value)
-  
+
 var keys = getKeys();
 var publicKey = keys[0];
 var privateKey = keys[1];
@@ -13,7 +13,7 @@ function refresh() {
     getBalance(keysT[0]);
     var txns = getTxns(keysT[0]);
     for (let i = 0; i <= txns.lenght(); i++) {
-        addTransaction(txns[i]);  
+        addTransaction(txns[i]);
     }
 }
 balaio = getBalance(publicKey);
@@ -25,8 +25,17 @@ function setNode(node) {
 }
 
 
+//Adds transaction to list    
+//type: 'sent' or 'received', party (str) = username of recipient, time (str) = time description, amountio(float) = amount in AIO
+//amountgbp = amountio * aioprice; 
 
+transactionList = document.getElementById("transactionList").innerHTML;
 
-
-    //Adds transaction to list    //type: 'sent' or 'received', party (str) = username of recipient, time (str) = time description, amountio(float) = amount in AIO
-    //amountgbp = amountio * aioprice; 
+function addTransaction(type, party, time, amountio) {
+    amountgbp = amountio * aioprice;
+    if (type == "sent") {
+        transactionList += '<div class="transaction"> <div class="transaction-icon"> <i class="fixicon" data-feather="arrow-up-right"></i> </div><div> <h4><b>' + party + '</b></h4> <h5 class="grey">' + time + '</h5> </div><div class="transaction-right"> <h4 class="danger nobottom"><b>-' + amountio + ' AIO</b></h4> <h5 class="grey transaction-label">-&pound;' + amountgbp + '</h5> </div></div>';
+    } else {
+        transactionList += '<div class="transaction"> <div class="transaction-icon green"> <i class="fixicon" data-feather="arrow-down-left"></i> </div><div> <h4><b>' + party + '</b></h4> <h5 class="grey">' + time + '</h5> </div><div class="transaction-right"> <h4 class="success"><b>+' + amountio + ' AIO</b></h4> <h5 class="grey transaction-label">&pound;4' + amountgbp + '</h5> </div></div></div>';
+    }
+}
