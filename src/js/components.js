e189d55fbef03b22d12a35b41a93abcf71a56f35
Ariel Hurdle <arielhurdle@gmail.com>
2019-09-15 12:19:22 +0100

    Update max gas system

--- src/js/components.js
+++ src/js/components.js
@@ -72,10 +72,9 @@ function updateEstimate(){
         message = message.substring(0,30);
         document.getElementById('message').value = message;
     }
-    
-    const estimation = ((2000 + (message.length * 60) / 100) * gasPrice;
-    
-    if (maxGas < (estimation/gasPrice)) {
+    let gasEstimation = ((2000 + (message.length * 60) / 100);
+    const estimation = gasEstimation * gasPrice;
+    if (maxGas < gasEstimation) {
         document.getElementById('sendWarning').style.display = 'block'; 
         document.getElementById('sendTransactionButton').disabled = true;
     } else {
